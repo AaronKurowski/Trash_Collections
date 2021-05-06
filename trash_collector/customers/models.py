eff6e1b322813d318264106fdd0cee0ec370df49
Aaron Kurowski <aaron12342@att.net>
2021-05-06 12:27:14 -0500

    Added properties to customer model.

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-# Create your models here.
-
-# TODO: Finish customer model by adding necessary properties to fulfill user stories
 
 
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    active = models.BooleanField(default=False)
+    weekday_pickup = models.CharField(max_length=50)
+    bonus_pickup = models.DateField(auto_now=False, auto_now_add=False)
+    amount_due = models.DecimalField(max_digits=None, decimal_places=2)
