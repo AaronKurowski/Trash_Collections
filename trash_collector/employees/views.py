1299f48650ef3783c321940438f2aaa573197df7
nmar30 <nmarcisso@live.com>
2021-05-10 17:11:12 -0400

    Added additional filter on index view to exclude non active users

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -11,10 +11,10 @@ from datetime import date
 
 
 def index(request):
-    my_date = date.today()
     # Get the Customer model from the other app, it can now be used to query the db
     Customer = apps.get_model('customers.Customer')
     todays_customers = Customer.objects.filter(weekly_pickup=convert_todays_date_to_day()) | Customer.objects.filter(bonus_pickup=date.today())
+    todays_customers = todays_customers.exclude(active=False)
     context = {'todays_customers': todays_customers}
     return render(request, 'employees/index.html', context)
 
@@ -22,3 +22,12 @@ def convert_todays_date_to_day():
     today = date.today()
     today = today.strftime("%A")
     return today
+
+def set_active():
+    # Function not complete
+    Customer = apps.get_model('customers.Customer')
+    todays_customers = Customer.objects.filter()
+    for customer in todays_customers:
+        if customer.suspend_start != None:
+            if customer.suspend_start <= date.today() and customer.suspend_end >= date.today():
+                customer.active = False
